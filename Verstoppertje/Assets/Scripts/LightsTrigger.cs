8f9ac48f9e49c8135dd77b23e5c23010b6bcebac
Nick Rau <nickrau3@gmail.com>
2020-06-10 15:36:57 +0200

    Initial commit
    Base project committed

---
+++ Verstoppertje/Assets/Scripts/LightsTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsTrigger : MonoBehaviour
+{
+    public bool use_X;
+    public GameObject doortrigger;
+    public GameObject player;
+
+    public GameObject lights_1;
+    public GameObject lights_2;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!use_X)
+        {
+            if(doortrigger.transform.position.z > player.transform.position.z)
+            {
+                Debug.Log("Player below");
+                lights_1.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("Player above");
+                lights_2.SetActive(true);
+            }
+        }
+        else 
+        {
+            if (doortrigger.transform.position.x > player.transform.position.x)
+            {
+                Debug.Log("Player left");
+                lights_1.SetActive(true);
+            }
+            else
+            {
+                Debug.Log("Player right");
+                lights_2.SetActive(true);
+            }
+        }
+
+    }
+}
---
+++ Verstoppertje/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    private Vector3 lastMoveDir;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        float speed = 3f;
+        float moveX = 0f;
+        float moveZ = 0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveZ = +1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveZ = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = +1f;
+        }
+
+        Vector3 moveDir = new Vector3(moveX, 0, moveZ).normalized;
+        Vector3 targetMovePos = transform.position + moveDir * speed * Time.deltaTime;
+        RaycastHit raycastHit;
+
+        if(Physics.Raycast(transform.position, moveDir, out raycastHit, speed * Time.deltaTime))
+        {
+            if (raycastHit.collider.gameObject.name.Contains("DoorTrigger"))
+            {
+                transform.position = targetMovePos;
+            }
+
+            Vector3 testMoveDir = new Vector3(moveDir.x, 0f).normalized;
+            Vector3 targetMovePos_2 = transform.position + testMoveDir * speed * Time.deltaTime;
+            bool raycastHit_2 = Physics.Raycast(transform.position, testMoveDir, speed * Time.deltaTime);
+
+            if (!raycastHit_2)
+            {
+                // Can move horizontal
+                transform.position = targetMovePos_2;
+            }
+            else
+            {
+                // Can't move horizontal
+                Vector3 testMoveDir_2 = new Vector3(0f, 0f, moveDir.z).normalized;
+                Vector3 targetMovePos_3 = transform.position + testMoveDir_2 * speed * Time.deltaTime;
+                bool raycastHit_3 = Physics.Raycast(transform.position, testMoveDir_2, speed * Time.deltaTime);
+
+                if (!raycastHit_3)
+                {
+                    // Can move vertically
+                    transform.position = targetMovePos_3;
+                }
+
+            }
+
+        }
+        else
+        {
+            // Player can move. No hits
+            transform.position = targetMovePos;
+        }
+    }
+}

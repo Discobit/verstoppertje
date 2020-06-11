3aad8c870cdb0f3d259f77a514bf800999e7f2bd
Nick Rau <nickrau3@gmail.com>
2020-06-11 02:01:17 +0200

    Update 3D Scene and Created Menus
    Updated player in 3D Scene with own flashlight that follows the mouse movement.
    
    Menu's are created with basic funtionality

---
+++ Verstoppertje/Assets/Scripts/LightMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMovement : MonoBehaviour
+{
+    public GameObject rb;
+    public Camera cam;
+    Vector2 mousePos;
+    public float offset;
+    //public float sensitivity;
+    Vector3 lookDir;
+
+    private void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            lookDir = hit.point - rb.transform.position;
+            Debug.DrawLine(rb.transform.position, hit.point);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float angle = Mathf.Atan2(lookDir.x, lookDir.z) * Mathf.Rad2Deg;
+        //Debug.Log(angle);
+        rb.transform.eulerAngles = new Vector3(0, angle, 0);
+    }
+}
--- Verstoppertje/Assets/Scripts/LightsTrigger.cs
+++ Verstoppertje/Assets/Scripts/LightsTrigger.cs
@@ -4,26 +4,29 @@ using UnityEngine;
 
 public class LightsTrigger : MonoBehaviour
 {
-    public bool use_X;
+    public bool use_left_right;
     public GameObject doortrigger;
     public GameObject player;
 
-    public GameObject lights_1;
-    public GameObject lights_2;
+    public GameObject lights_left;
+    public GameObject lights_right;
+
+    public GameObject lights_top;
+    public GameObject lights_down;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!use_X)
+        if (!use_left_right)
         {
             if(doortrigger.transform.position.z > player.transform.position.z)
             {
                 Debug.Log("Player below");
-                lights_1.SetActive(true);
+                lights_top.SetActive(true);
             }
             else
             {
                 Debug.Log("Player above");
-                lights_2.SetActive(true);
+                lights_down.SetActive(true);
             }
         }
         else 
@@ -31,12 +34,12 @@ public class LightsTrigger : MonoBehaviour
             if (doortrigger.transform.position.x > player.transform.position.x)
             {
                 Debug.Log("Player left");
-                lights_1.SetActive(true);
+                lights_right.SetActive(true);
             }
             else
             {
                 Debug.Log("Player right");
-                lights_2.SetActive(true);
+                lights_left.SetActive(true);
             }
         }
 

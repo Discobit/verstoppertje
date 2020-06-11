ee3a081233b8439bf9acb51853fc58181ec3331e
Nick Rau <nickrau3@gmail.com>
2020-06-11 03:31:24 +0200

    Added ClientSocketConnector and added additional Menu's
    Added a menu for creating or joining a game. Made a prefab and Script which keeps the SocketConnector running though the entire game. Functions are created, but need to be connected to the buttons and input fields need to be added

---
+++ Verstoppertje/Assets/Scripts/ClientSocketAccesser.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientSocketAccesser : MonoBehaviour
+{
+    public static ClientSocketAccesser Instance;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Startup()
+    {
+        ISocket.StartSocket("", 34000);
+    }
+
+    public void Login()
+    {
+        string username = "";
+        string password = "";
+
+        ISocket.login(username, password);
+        //string response = ISocket.ResponseListener(); //Add connection responsetype
+    }
+
+    public void Register()
+    {
+        string username = "";
+        string password = "";
+        ISocket.register(username, password);
+        //string response = ISocket.ResponseListener(); //Add connection responsetype
+    }
+
+    public void GameCreate()
+    {
+        string username = "";
+        string password = "";
+        ISocket.gamecreate(username, password);
+        //string response = ISocket.ResponseListener(); //Add connection responsetype
+    }
+
+    public void GameAccess()
+    {
+        string username = "";
+        string password = "";
+        ISocket.gameaccess(username);
+        //string response = ISocket.ResponseListener(); //Add connection responsetype
+    }
+
+    public void Logout()
+    {
+        string username = "";
+        string password = "";
+        ISocket.logout(username);
+        //string response = ISocket.ResponseListener(); //Add connection responsetype
+    }
+
+    public void ChangePlayerType()
+    {
+        int gameid = 0;
+        ISocket.changeplayertype(gameid);
+        //string response = ISocket.ResponseListener(); //Add connection responsetype
+    }
+
+    public void HiderHidden()
+    {
+        int gameid = 0;
+        ISocket.hiderhidden(gameid);
+        //string response = ISocket.ResponseListener(); //Add connection responsetype
+    }
+
+    public void EnterRoom()
+    {
+        int gameid = 0;
+        int roomnum = 0;
+        ISocket.roomenter(gameid, roomnum);
+        //string response = ISocket.ResponseListener(); //Add connection responsetype
+    }
+
+    public void SeekerDone()
+    {
+        int gameid = 0;
+        ISocket.seekerdone(gameid);
+        //string response = ISocket.ResponseListener(); //Add connection responsetype
+    }
+
+}
